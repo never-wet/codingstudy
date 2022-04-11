f9626b06acbfaaa705b74cbed3824d69d2cc63f2
Junha Park <96017487+junha2010@users.noreply.github.com>
2022-04-10 23:06:13 -0400

    게임 만드는 중...

---
+++ python/계산기.py
@@ -0,0 +1,25 @@
+
+
+def sum():
+           a = input()
+           b = input()
+           print(int(a) + int(b))
+
+def sub():
+           a = input()
+           b = input()
+           print(int(a) - int(b))
+
+
+def mul():
+           a = input()
+           b = input()
+           print(int(a) * int(b))
+
+
+def div():
+           a = input()
+           b = input()
+           print(int(a) / int(b))
+
+div()
